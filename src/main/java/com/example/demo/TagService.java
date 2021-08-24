4b8491a99b6096cb76a0f8d7a1182cfb367e19b7
ZachSinger <32113115+ZachSinger@users.noreply.github.com>
2021-08-24 13:46:35 -0400

    added Tag class, repo, service and controller, tested and confirmed working in Postman (#5)
    Co-authored-by: Zach <zach@zipcodes-MacBook-Pro-6.local>

---
+++ src/main/java/com/example/demo/TagService.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TagService {
+    @Autowired
+    private TagRepository repository;
+
+    public Tag create(Tag tag){ return repository.save(tag); }
+
+    public Tag read(Long id){ return repository.findById(id).get(); }
+
+    public List<Tag> readAll(){
+        Iterable<Tag> tagIterable = repository.findAll();
+        List<Tag> result = new ArrayList<>();
+        tagIterable.forEach(result::add);
+        return result;
+    }
+
+    public Tag update(Long id, Tag newTag){
+        Tag tagInDatabase = read(id);
+        tagInDatabase.setName(newTag.getName());
+        return repository.save(tagInDatabase);
+    }
+
+    public Tag delete(Tag tag){
+        repository.delete(tag);
+        return tag;
+    }
+
+    public Tag delete(Long id){
+        return delete(read(id));
+    }
+}
