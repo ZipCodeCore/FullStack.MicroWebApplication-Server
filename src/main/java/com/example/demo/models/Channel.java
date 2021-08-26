09441c5a8fb161d5efb33303c3f4e611b9dad665
laura <laurab90>
2021-08-26 12:10:02 -0400

    message

--- src/main/java/com/example/demo/models/Channel.java
+++ src/main/java/com/example/demo/models/Channel.java
@@ -1,50 +1,5 @@
 package com.example.demo.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.util.List;
-
-@Entity
 public class Channel {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String name;
-    private List<Profile> profileList;
-    private List<Message> messages;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Profile> getProfileList() {
-        return profileList;
-    }
-
-    public void setProfileList(List<Profile> profileList) {
-        this.profileList = profileList;
-    }
-
-    public List<Message> getMessages() {
-        return messages;
-    }
-
-    public void setMessages(List<Message> messages) {
-        this.messages = messages;
-    }
 }
--- src/main/java/com/example/demo/repository/MessageRepo.java
+++ src/main/java/com/example/demo/repository/MessageRepo.java
@@ -1,4 +1,10 @@
 package com.example.demo.repository;
 
-public class MessageRepo {
+import com.example.demo.models.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepo extends JpaRepository<Message, Long> {
+
 }
